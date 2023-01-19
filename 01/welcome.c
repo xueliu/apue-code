a05ae80a2d18cda096a6e9ca084b5ca2cdcac452
Xue Liu <liuxueliuxuenetmail@gmail.com>
2023-01-19 16:20:21 +0100

    Update welcome.c

--- 01/welcome.c
+++ 01/welcome.c
@@ -17,5 +17,5 @@
 
 int
 main() {
-	printf("Welcome to CS631 Advanced Programming in the UNIX Environment, %s!\n", getlogin())
+	printf("Welcome to CS631 Advanced Programming in the UNIX Environment, %s!\n", getlogin());
 }
